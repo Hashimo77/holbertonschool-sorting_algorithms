128fd5604b97a683b87bc1ad2dd83df803c4d2d2
Hashimo77 <hagamirz7@gmail.com>
2025-09-26 07:39:01 +0000

    asjdha

---
+++ sort.h
@@ -0,0 +1,16 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+/* Print helpers (verilmişdir) */
+void print_array(const int *array, size_t size);
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
